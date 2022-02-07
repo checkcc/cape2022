bbf17512cdfa3408288949b31cbef2bb5500ac66
unknown <awsdev747@gmail.com>
2022-02-07 10:16:06 +0530

    thread demo

---
+++ Day 1/Threads/NamingThread.java
@@ -0,0 +1,27 @@
+package com.demo.threads;
+
+class NamingThreadDemo extends Thread {
+
+	// predefind method
+
+	public void run() {
+
+		System.out.println("running..");
+	}
+
+}
+
+public class NamingThread {
+
+	public static void main(String ar[]) {
+		
+		NamingThreadDemo t1 = new NamingThreadDemo();
+		
+		t1.start();
+		t1.setName(" tom-thread");
+		System.out.println("get the name of thread" + t1.getName());
+		t1.setPriority(8);
+
+	}
+
+}
